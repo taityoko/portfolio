9eb744c1f17667950a155beffe2707bfc1a8f546
taityoko <tai1503ti@outlook.jp>
2018-07-25 02:44:05 +0000

    Finish user signup

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,6 +13,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
        
     get signup_path
-    assert_select "title", full_title("Sigh up")
+    assert_select "title", full_title("Sign up")
   end
 end
