d4c9fe6724e63f479fdbaa8f2368fe1b6b1ccdf8
taityoko <tai1503ti@outlook.jp>
2018-08-05 22:05:28 +0000

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -69,7 +69,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+  test "authenticated? should return false for a user nil digest" do
+    assert_not @user.authenticated?(:remember , '')
   end
 end
